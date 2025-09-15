9a65818596fa156344b233bbb30de9296618e1c0
xexobi <fadlidev99@gmail.com>
2025-09-15 22:47:15 +0700

    vite , react , tailwincss, nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Contoh project Python pakai Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            echo "Welcome to nix develop"
+          '';
+        };
+      }
+    );
+}
